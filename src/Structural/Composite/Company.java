63daa8aea079d877f30a7fb673fb00a5b55984e4
Balan Adelin-Cristian <adelincristian949@yahoo.com>
2021-09-24 00:23:28 +0300

    Added missing comments in some pattern examples.

--- src/Structural/Composite/Company.java
+++ src/Structural/Composite/Company.java
@@ -2,6 +2,24 @@ package Structural.Composite;
 
 public class Company {
 
+     /**
+      *                  company
+      *                 /       \
+      *         department1     department2
+      *         /       \              \
+      *   employee1  employee2         employee3
+      *
+      *  This design pattern allows us to create tree structured objects, which are grouped by certain properties.
+      *
+      * Checklist:
+      *
+      * 1. Make sure that you can apply tree-like structure for your components
+      * 2. Create unified interface that will combine operations on the group of objects and on the single object.
+      * 3. Create class of single object
+      * 4. Create class for group of objects that implement the same unified interface.
+      * 5. Add operations to add and remove components into the container
+      */
+
     public static void main(String[] args) {
         Employee dev1 = new Developer( "Name1", 1,"Pro dev");
         Employee dev2 = new Developer( "Name2", 2,"Pro dev");
--- src/Structural/Flyweight/FlyWightDemo.java
+++ src/Structural/Flyweight/FlyWightDemo.java
@@ -1,6 +1,21 @@
 package Structural.Flyweight;
 
 public class FlyWightDemo {
+
+    /**
+     * This pattern is primarily used to reduce the amount of objects created, so in consequence, to reduce
+     * the memory usage and increase performance. It tries to reuse already existing similar objects.
+     *
+     * Checklist:
+     *
+     * 1. Split state of our object in two groups - inner and outer state.
+     * 2. Create a type where constant fields will be initialized through the constructor.
+     * 3. Make sure that outer state may be passed as method arguments
+     * 4. Create factory that will cache state and will return already created objects. Client should
+     * request object with specific state but not created directly.
+     * 5. Client should store outer state, or generate it and pass it as an argument to a flyweight.
+     */
+
     public static void main(String[] args) {
         RobotFactory robotFactory = new RobotFactory();
 
